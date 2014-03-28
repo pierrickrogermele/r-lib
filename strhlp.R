d5eb3a8cb722646ba8ed671d1bd11a9a8b6d6058
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2014-03-28 09:48:55 +0100

    Add split function.

--- strhlp.R
+++ strhlp.R
@@ -10,3 +10,19 @@ trim.trailing <- function (x) sub("\\s+$", "", x)
 
 # Trim leading and trailing whitespaces
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+
+#############
+# SPLITTING #
+#############
+
+# s         The string to split.
+# sep       The separator on which to split.
+# trim      Trim whitespaces for the resulting elements.
+# unlist    Unlist the result, So that for a single string (i.e.: s has length 1), it returns a vector of strings instewad of a list of vectors of strings.
+# RETURN    A list of strings.
+split <- function(s, sep, trim = TRUE, unlist = FALSE) {
+	v <- strsplit(s, sep)
+	if (trim) v <- lapply(v, trim)
+	if (unlist) v <- unlist(v)
+	return(v)
+}
