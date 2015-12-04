6d2936c05f501a265b4d5ff6dabc54fd5d803968
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2015-12-04 23:30:07 +0100

    Retry query in failure case.

--- UrlRequestScheduler.R
+++ UrlRequestScheduler.R
@@ -3,7 +3,7 @@
 # CLASS DECLARATION #
 #####################
 
-UrlRequestScheduler <- setRefClass("UrlRequestScheduler", fields = list(n = "numeric", t = "numeric", time_of_last_request = "ANY", useragent = "character", .ssl.verifypeer = "logical"))
+UrlRequestScheduler <- setRefClass("UrlRequestScheduler", fields = list(.n = "numeric", .t = "numeric", .time.of.last.request = "ANY", .useragent = "character", .ssl.verifypeer = "logical", .nb.max.tries = "integer", .verbose = "integer"))
 
 # n: number of connections
 # t: time (in seconds)
@@ -15,11 +15,13 @@ UrlRequestScheduler <- setRefClass("UrlRequestScheduler", fields = list(n = "num
 ###############
 
 UrlRequestScheduler$methods( initialize = function(n = 1, t = 1, useragent = NA_character_, ssl.verifypeer = TRUE, ...) {
-	n <<- n
-	t <<- t
-	time_of_last_request <<- -1
-	useragent <<- useragent
+	.n <<- n
+	.t <<- t
+	.time.of.last.request <<- -1
+	.useragent <<- useragent
+	.nb.max.tries <<- 10L
 	.ssl.verifypeer <<- ssl.verifypeer
+	.verbose <<- 0L
 	callSuper(...) # calls super-class initializer with remaining parameters
 })
 
@@ -28,7 +30,15 @@ UrlRequestScheduler$methods( initialize = function(n = 1, t = 1, useragent = NA_
 ##################
 
 UrlRequestScheduler$methods( setUserAgent = function(useragent) {
-	useragent <<- useragent
+	.useragent <<- useragent
+})
+
+###############
+# SET VERBOSE #
+###############
+
+UrlRequestScheduler$methods( setVerbose = function(verbose) {
+	.verbose <<- verbose
 })
 
 ##################
@@ -36,20 +46,20 @@ UrlRequestScheduler$methods( setUserAgent = function(useragent) {
 ##################
 
 # Wait the specified between two requests.
-UrlRequestScheduler$methods( .wait_as_needed = function() {
+UrlRequestScheduler$methods( .wait.as.needed = function() {
 
 	# Compute minimum waiting time between two URL requests
-	waiting_time <- .self$t / .self$n
+	waiting_time <- .self$.t / .self$.n
 
 	# Wait, if needed, before previous URL request and this new URL request.
-	if (.self$time_of_last_request > 0) {
-		spent_time <- Sys.time() - .self$time_of_last_request
+	if (.self$.time.of.last.request > 0) {
+		spent_time <- Sys.time() - .self$.time.of.last.request
 		if (spent_time < waiting_time)
 			Sys.sleep(waiting_time - spent_time)
 	}
 
 	# Store current time
-	time_of_last_request <<- Sys.time()
+	.time.of.last.request <<- Sys.time()
 })
 
 ####################
@@ -57,7 +67,7 @@ UrlRequestScheduler$methods( .wait_as_needed = function() {
 ####################
 
 UrlRequestScheduler$methods( .get_curl_opts = function(url) {
-	opts <- curlOptions(useragent = .self$useragent, timeout.ms = 60000, verbose = FALSE)
+	opts <- curlOptions(useragent = .self$.useragent, timeout.ms = 60000, verbose = FALSE)
 	return(opts)
 })
 
@@ -65,15 +75,12 @@ UrlRequestScheduler$methods( .get_curl_opts = function(url) {
 # GET URL #
 ###########
 
-UrlRequestScheduler$methods( getUrl = function(url, params = NULL, method = 'GET') {
+UrlRequestScheduler$methods( .doGetUrl = function(url, params = NULL, method = 'GET') {
 
 	library(bitops)
 	library(RCurl)
 
 	content <- NA_character_
-
-	# Wait required time between two requests
-	.self$.wait_as_needed()
 
 	# Use form to send URL request
 	if ( ! is.null(params) && ! is.na(params))
@@ -86,6 +93,24 @@ UrlRequestScheduler$methods( getUrl = function(url, params = NULL, method = 'GET
 	# Get URL normally
 	else
 		content <- getURL(url, .opts = .self$.get_curl_opts(), ssl.verifypeer = .self$.ssl.verifypeer)
+
+	return(content)
+})
+
+UrlRequestScheduler$methods( getUrl = function(url, params = NULL, method = 'GET') {
+
+	content <- NA_character_
+
+	# Wait required time between two requests
+	.self$.wait.as.needed()
+
+	# Run query
+	for (i in seq(.self$.nb.max.tries)) {
+		tryCatch({ content <- .self$.doGetUrl(url, params = params, method = method) },
+		         error = function(e) { if (.self$.verbose > 0) print("Retry connection to server...") } )
+		if ( ! is.na(content))
+			break
+	}
 
 	return(content)
 })
