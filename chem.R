4440f562f14063d16c1f69de33fcf909e8183d7e
Pierrick Roger Mele <pierrick.rogermele@cea.fr>
2016-01-15 19:26:42 +0100

    Update java-chem access (use maven now).

--- chem.R
+++ chem.R
@@ -1,21 +1,38 @@
 if ( ! exists('load.sdf')) { # Do not load again if already loaded
 
-	r.lib.chem.file.path <- parent.frame(2)$ofile
+	#############
+	# CONSTANTS #
+	#############
+
+	R.LIB.CHEM.FILE.PATH <- parent.frame(2)$ofile
 	
+	##################
+	# LOAD JAVA CHEM #
+	##################
+
+	load.java.chem <- function() {
+		library(rJava)
+		.jinit()
+		cmd <- c("mvn", "-f", file.path(dirname(R.LIB.CHEM.FILE.PATH), '..', 'java-chem'), "org.apache.maven.plugins:maven-dependency-plugin:2.10:build-classpath")
+		classpath <- system(paste(cmd, collapse = " "), intern = TRUE)
+		classpath <- grep("^\\[INFO]", classpath, invert = TRUE, value = TRUE)
+		classpath <- strsplit(classpath, split = ':')[[1]] # TODO make it portable (classpath under Windows use ';' instead of ':')
+		.jaddClassPath(classpath)
+		.jaddClassPath(file.path(dirname(R.LIB.CHEM.FILE.PATH), '..', 'java-chem', 'target', 'java-chem-1.0.jar'))
+	}
+
 	#############
 	# GET INCHI #
 	#############
 
 	get.inchi <- function(mol) {
-		library(rJava)
-		.jinit()
-		.jaddClassPath(file.path(dirname(r.lib.chem.file.path), '..', 'java-chem', 'fr.cea.chem.debug.jar'))
-		.jaddClassPath(file.path(dirname(r.lib.chem.file.path), '..', 'java-cdk', 'cdk-1.5.10.jar'))
-		cdkhlp <- .jnew('fr/cea/chem/CdkHelper')
+		load.java.chem()
+		cdkhlp <- .jnew('org/openscience/chem/CdkHelper')
 		inchi <- .jcall(cdkhlp, 'S', 'getInchi', mol)
 		return(inchi)
 	}
 
+	# 
 	############
 	# LOAD SDF #
 	############
@@ -75,11 +92,8 @@ if ( ! exists('load.sdf')) { # Do not load again if already loaded
 		close(con)
 
 		# Load molecule structures
-		library(rJava)
-		.jinit()
-		.jaddClassPath(file.path(dirname(r.lib.chem.file.path), '..', 'java-chem', 'fr.cea.chem.debug.jar'))
-		.jaddClassPath(file.path(dirname(r.lib.chem.file.path), '..', 'java-cdk', 'cdk-1.5.10.jar'))
-		cdkhlp <- .jnew('fr/cea/chem/CdkHelper')
+		load.java.chem()
+		cdkhlp <- .jnew('org/openscience/chem/CdkHelper')
 		struct <- .jcall(cdkhlp, '[Lorg/openscience/cdk/interfaces/IAtomContainer;', 'loadSdf', file)
 
 		return(list(struct = struct, info = info))
